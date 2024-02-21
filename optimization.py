c935347533d3a0b8ae50386c90cf1e410746c726
CuriousLocky <835322750jm37@outlook.com>
2024-02-21 15:53:16 +0900

    Change debug message format

--- optimization.py
+++ optimization.py
@@ -4,12 +4,12 @@ from abc import abstractmethod, ABC
 class Optimization(ABC):
     @abstractmethod
     def estimate_flash_overhead(self) -> int:
-        pass
+        raise NotImplementedError("Optimization.estimate_flash_overhead")
 
     @abstractmethod
     def estimate_sram_overhead(self) -> int:
-        pass
+        raise NotImplementedError("Optimization.estimation_sram_overhead")
 
     @abstractmethod
     def estimate_latency_overhead(self) -> int:
-        pass
+        raise NotImplementedError("Optimization.estimate_latency_overhead")
