ee54683052702f081353339cdc65f7a50207407f
CuriousLocky <835322750jm37@outlook.com>
2024-02-21 16:08:40 +0900

    Change optimization package layout

--- optimization/__init__.py
+++ optimization/__init__.py
@@ -6,15 +6,17 @@ from ir.model import Model
 class OptimizationOption(ABC):
     @abstractmethod
     def estimate_flash_overhead(self) -> int:
-        pass
+        raise NotImplementedError("OptimizationOption.estimate_flash_overhead")
 
     @abstractmethod
     def estimate_sram_overhead(self) -> int:
-        pass
+        raise NotImplementedError(
+            "OptimizationOption.estimation_sram_overhead")
 
     @abstractmethod
     def estimate_latency_overhead(self) -> int:
-        pass
+        raise NotImplementedError(
+            "OptimizationOption.estimate_latency_overhead")
 
     @abstractmethod
     def do_optimization(self, model: Model) -> Model:
