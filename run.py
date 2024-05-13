a48a458b275369b2416d15d1f99e084b60358807
CuriousLocky <835322750jm37@outlook.com>
2024-05-13 16:23:42 +0900

    Add vww example model

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from shan_frame import compile_model_at
 
-model_path = "./example/ad_small_int8.tflite"
+model_path = "./example/mcunet-5fps_vww.tflite"
 output_dir = "./out"
 
 sram_usage = compile_model_at(
