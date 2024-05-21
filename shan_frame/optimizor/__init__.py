8912fd8cf21da01d25cd5d4b9059745577fe7028
CuriousLocky <835322750jm37@outlook.com>
2024-05-21 16:47:23 +0900

    Add data layout determination

--- shan_frame/optimizor/__init__.py
+++ shan_frame/optimizor/__init__.py
@@ -1,6 +1,6 @@
 
-from ..ir import Model
-from ..ir.operator import Conv2D, DepthConv2D
+from ..ir import DataLayout, Model
+from ..ir.operator import Conv2D, DepthConv2D, Add, Mul
 from .mem_scheduler import MemoryScheduler
 from .visualizer import visualize_memory
 
@@ -44,6 +44,51 @@ class Optimizer:
         final_peak_mem = self.mem_scheduler.schedule(self.model)
         visualize_memory(self.model)
         
+        # determine optimal data layout for intermediate tensors
+        self.set_data_layout()
+        
         return (self.model, final_peak_mem)
     
+    def set_data_layout(self):
+        # set all output layout to default as HWC
+        for op in self.model.operators.values():
+            output = self.model.tensors[op.output_idx]
+            output.layout = DataLayout.HWC
             
+        # set first input to HWC for compatibility
+        first_input_idx = self.model.operators[0].input_idx_list[0]
+        self.model.tensors[first_input_idx].layout = DataLayout.HWC
+        
+        # set input layout for depthwise conv2d
+        for op in reversed(self.model.operators.values()):
+            if not isinstance(op, DepthConv2D):
+                continue
+            input_tensor = self.model.tensors[op.input_idx]
+            output_tensor = self.model.tensors[op.output_idx]
+            # overlapped input and output must have the layout
+            if op.io_overlap:
+                input_tensor.layout = output_tensor.layout
+            else:
+                input_tensor.layout = DataLayout.CHW
+            
+        # check data layout alignment for ADD and MUL
+        for op in self.model.operators.values():
+            if not (isinstance(op, Add) or isinstance(op, Mul)):
+                continue
+            input0 = self.model.tensors[op.input_idx[0]]
+            input1 = self.model.tensors[op.input_idx[1]]
+            # if not the same layout, set all to HWC
+            if input0.layout != input1.layout:
+                input0.layout = DataLayout.HWC
+                input1.layout = DataLayout.HWC
+        
+        # double check overlapped input and output layout
+        for op in reversed(self.model.operators.values()):
+            if not (isinstance(op, Conv2D) or isinstance(op, DepthConv2D)):
+                continue
+            input_tensor = self.model.tensors[op.input_idx]
+            output_tensor = self.model.tensors[op.output_idx]
+            # overlapped input and output must have the layout
+            if op.io_overlap:
+                input_tensor.layout = output_tensor.layout
+        
